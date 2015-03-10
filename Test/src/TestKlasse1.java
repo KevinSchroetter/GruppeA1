e0ec61d5c78b1c39458dcbcbbd8ccc2273009802
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2015-03-11 00:19:57 +0100

    Ein weiterer TestCommit. Dieser ist als ein Commit für eine "ZERSTÖRTE" Version zu sehen. Ziel des Tests ist es, irgendwie die vorherige Version wieder zu bekommen

--- Test/src/TestKlasse1.java
+++ Test/src/TestKlasse1.java
@@ -4,6 +4,7 @@ public class TestKlasse1 {
 	public static void main(String[] args) {
 		System.out.println("Ich bin ein Testprojekt");
 		System.out.println("Eigene Änderung. Hochladen diesmal ohne PushBranch, sondern via Upstream");
+		System.out.println("uuund ein weiterer Test. Wie greife ich konkret auf eine ältere Funktion zu");
 
 	}
 
