a6422fe9c3d48bbfd57d178b3aab6d708c5d428a
AnnaRosa <Anna-Chiara.Rosa@reutlingen-university.de>
2015-03-11 00:47:43 +0100

    Code um Frage erweitert

--- Test/src/TestKlasse1.java
+++ Test/src/TestKlasse1.java
@@ -6,6 +6,7 @@ public class TestKlasse1 {
 		System.out.println("Eigene Änderung. Hochladen diesmal ohne PushBranch, sondern via Upstream");
 		System.out.println("uuund ein weiterer Test. Wie greife ich konkret auf eine ältere Funktion zu");
 		System.out.println("Direkter Edit über github.com als vorschub vor der Geschichte mit der Version..");
+		System.out.println("Was hast du geänadert?");
 
 	}
 
