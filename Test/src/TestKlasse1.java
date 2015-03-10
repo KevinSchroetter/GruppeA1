deabfa5965e98444e43afa2206a3fc2c63c02ce3
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2015-03-11 00:09:05 +0100

    Testcommit 2 nach Änderung mit Upstream

--- Test/src/TestKlasse1.java
+++ Test/src/TestKlasse1.java
@@ -3,6 +3,7 @@ public class TestKlasse1 {
 
 	public static void main(String[] args) {
 		System.out.println("Ich bin ein Testprojekt");
+		System.out.println("Eigene Änderung. Hochladen diesmal ohne PushBranch, sondern via Upstream");
 
 	}
 
