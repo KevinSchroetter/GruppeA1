53a8e8755c1143df1e254f44bda0bbd5224faae5
AnnaRosa <Anna_Chiara.Rosa@Student.Reutlingen-University.de>
2015-03-10 23:59:14 +0100

    Neuer Text

--- ErstesGit/src/HalloGit.java
+++ ErstesGit/src/HalloGit.java
@@ -3,6 +3,7 @@ public class HalloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo, ich bin ganz neuer Code...");
+		System.out.println("Hallo neuer Code <3");
 	}
 
 }
