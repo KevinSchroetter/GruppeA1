59dd5cf39b13a303b34b06559c5850b85b355445
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2015-03-11 16:01:55 +0100

    Verständnistest: Was wird alles gepusht und was wird alles gepullt?

---
+++ ErstesGit/src/HalloGit2.java
@@ -0,0 +1,9 @@
+
+public class HalloGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test 1: PUSHPUSHPUSH");
+
+	}
+
+}
---
+++ Test/src/TestKlasse2.java
@@ -0,0 +1,9 @@
+
+public class TestKlasse2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test 1: PUSHPUSHPUSH");
+
+	}
+
+}
