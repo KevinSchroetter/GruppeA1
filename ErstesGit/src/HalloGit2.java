af79b639953a79a07bede3cb69611563987e7faa
Alex Brueckner <Alexander.Brueckner@Student.Reutlingen-University.de>
2015-03-11 16:08:19 +0100

    Läuft!

--- ErstesGit/src/HalloGit2.java
+++ ErstesGit/src/HalloGit2.java
@@ -3,6 +3,7 @@ public class HalloGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test 1: PUSHPUSHPUSH");
+		System.out.println("PULLPULLPULL Erfolgreich - jetzt auch PUSH OPUSH PUSH!");
 
 	}
 
