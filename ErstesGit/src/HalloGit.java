0c83cf51fd7217c021655efb2fa328e00742d3b6
Alex Brueckner <Alexander.Brueckner@Student.Reutlingen-University.de>
2015-03-11 15:53:11 +0100

    Heureka es geht! Hoff ich....

--- ErstesGit/src/HalloGit.java
+++ ErstesGit/src/HalloGit.java
@@ -4,6 +4,7 @@ public class HalloGit {
 	public static void main(String[] args) {
 		System.out.println("Hallo, ich bin ganz neuer Code...");
 		System.out.println("Hallo neuer Code <3");
+		System.out.println("Brückner kann jetzt auch pushen!");
 	}
 
 }
