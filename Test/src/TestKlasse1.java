3e80593602abb250b58a3afb760ecd04acf5a388
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2015-03-11 15:36:40 +0100

    Testcommit fürs Team

--- Test/src/TestKlasse1.java
+++ Test/src/TestKlasse1.java
@@ -7,6 +7,7 @@ public class TestKlasse1 {
 		System.out.println("uuund ein weiterer Test. Wie greife ich konkret auf eine ältere Funktion zu");
 		System.out.println("Direkter Edit über github.com als vorschub vor der Geschichte mit der Version..");
 		System.out.println("Was hast du geänadert?");
+		System.out.println("noch ma n push-test. Alex hat probleme mit dem pushen und pullen geht bei ihm auch nicht");
 
 	}
 
