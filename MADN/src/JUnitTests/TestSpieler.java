f0b73a46ea8436bf6423dabf9e8a7fe40a88788c
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2015-03-12 23:15:48 +0100

    -Neues Package "JUnitTests" für Grey Box Tests angelegt. -Neuen JUnit Test Case "TestSpieler" angelegt.

---
+++ MADN/src/JUnitTests/TestSpieler.java
@@ -0,0 +1,19 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.After;
+import org.junit.AfterClass;
+
+public class TestSpieler {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
