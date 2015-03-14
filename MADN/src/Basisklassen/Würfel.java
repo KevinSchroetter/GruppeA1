36cdbddec14fe8700ea5594730d9990c5a1c7604
AnnaRosa <Anna_Chiara.Rosa@Student.Reutlingen-University.de>
2015-03-14 20:53:09 +0100

    Funktionierende Würfel-Klasse und Würfel-Test-Klasse

--- MADN/src/Basisklassen/Würfel.java
+++ MADN/src/Basisklassen/Würfel.java
@@ -1,5 +1,28 @@
 package Basisklassen;
 
+/** Dies ist die Klasse Würfel für das MADN-Spiel.
+ * Die Klasse besitzt die Eigenschaft gewürfelteZahl, in der sie die zuletzt mit der werfen-Methode gewürfelte Zahl speichert.
+@author Anna Rosa
+@version 1.0
+*/
+
 public class Würfel {
 
+	private int augenzahl;
+	/**
+	 * Eine Methode, die die zuletzt gewürfelte Zahl zurückgibt.
+	 * @return augenzahl
+	 */
+	public int getAugenzahl(){
+		return augenzahl;
+	} 
+	
+	/**
+	 * Eine Methode, die einem Würfel entsprechend eine Zufallsganzzahl zwischen 1 und 6 zurückgibt.
+	 * @return augenzahl
+	 */
+	public int werfen(){
+		int augenzahl = (int)  ((Math.random()*6) + 1);
+		return augenzahl;
+	}
 }
---
+++ MADN/src/JUnitTests/WürfelTest.java
@@ -0,0 +1,41 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.After;
+import org.junit.AfterClass;
+import Basisklassen.*;
+
+public class WürfelTest {
+	
+	public static Würfel würfel;
+	
+	@BeforeClass
+	public static void erstelleWürfel(){
+		würfel=new Würfel();
+	}
+	
+	@Before
+	public void ausgabeVorher(){
+		System.out.println("Test gestartet");
+	}
+	
+	@After 
+	public void ausgabeNachher(){
+		System.out.println("Test beendet");
+	}
+	
+	
+	@Test
+	public void test() {
+			int erg=würfel.werfen();
+			assertTrue(erg==1|erg==2|erg==3|erg==4|erg==5|erg==6);
+		
+		
+	}
+
+}
