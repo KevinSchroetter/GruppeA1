129239c03d6aa6e6726e925d221c8455e8f0b968
FelixRosa <Felix_Frederic.Rosa@Student.Reutlingen-University.de>
2015-03-17 00:14:19 +0100

    MergeRücktest

--- Test/src/MergenTest.java
+++ Test/src/MergenTest.java
@@ -4,6 +4,8 @@ public class MergenTest {
 		System.out.println("Hallo, ich bin Anna");
 		System.out.println("Ich mag Kekse");
 		System.out.println("Wer ist der weltbeste Merger?");
+		System.out.println("Felix ist es");
+		  System.out.println("Mergen ist toll");
 	}
 
 }
