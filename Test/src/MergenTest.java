eab191350673a68bf9086203b0a7be7a3b60181a
AnnaRosa <Anna_Chiara.Rosa@Student.Reutlingen-University.de>
2015-03-17 00:11:24 +0100

    Mergen Testklasse

---
+++ Test/src/MergenTest.java
@@ -0,0 +1,9 @@
+
+public class MergenTest {
+	public static void main(String[] args){
+		System.out.println("Hallo, ich bin Anna");
+		System.out.println("Ich mag Kekse");
+		System.out.println("Wer ist der weltbeste Merger?");
+	}
+
+}
