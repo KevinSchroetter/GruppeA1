0285699641316472f4452fc1dfd81884e263465b
AnnaRosa <Anna_Chiara.Rosa@Student.Reutlingen-University.de>
2015-03-30 00:41:01 +0200

    Changelog: Hinzufügen der defensiven KI

--- MADN/src/Basisklassen/Spieler.java
+++ MADN/src/Basisklassen/Spieler.java
@@ -200,15 +200,46 @@ public class Spieler {
 
 		}
 	}
-
+	
+	/**
+	 * Elementklasse der Klasse Spieler, erbt von KI. Dies ist die defensiv agierende KI-Klasse.
+	 * @author Anna Rosa
+	 *
+	 */
 	public class KI_Defensiv extends KI {
-
+		/**
+		 * Konstruktor der defensiven KI, wird durch den Spieler-Konstruktor aufgerufen.
+		 * @param s-Spieler, der eine KI werden soll
+		 */
 		public KI_Defensiv(Spieler s) {
 			super(s);
 		}
-		
+		/**
+		 * Methode, die würfelt und dann nach den Prioritäten entscheidet, welche Figur laufen soll.
+		 * Mögliche Fälle: 1. Es ist mindestens eine Figur im Spiel, dann läuft die Spielfigur, die noch im
+		 * Spiel und schon am Weitesten gelaufen ist, 2. es ist keine Figur aktiv im Spiel, dann wird 
+		 * gespawnt. Die dritte Priorität schlagen kann vernachlässigt werden, da entweder die weiteste
+		 * Figur zieht, egal ob sie schlagen kann oder nicht, oder irgendeinen Figur spawnt und mit Glück dabei
+		 * schlägt.
+		 */
 		public void zugWählen(){
-			
+			int augenzahl = iB.rollTheDice();
+			ArrayList<Spielfigur> kannZiehen = iB.ausgabeZugFiguren();
+			Spielfigur[] amWeitesten = new Spielfigur[kannZiehen.size()];
+			for (int i = 0; i < kannZiehen.size(); i++) {
+				amWeitesten[i] = kannZiehen.get(i);
+			}
+			for (int i = 0; i < amWeitesten.length; i++) {
+				if (amWeitesten[i].getFelderGelaufen() > amWeitesten[i + 1]
+						.getFelderGelaufen()) {
+					Spielfigur temp = amWeitesten[i + 1];
+					amWeitesten[i] = amWeitesten[i + 1];
+					amWeitesten[i + 1] = temp;
+				}
+			}
+			String id = "" + amWeitesten[amWeitesten.length - 1].getID();
+			iB.zugDurchführen(id, augenzahl);
+						
 		}
 
 	}
