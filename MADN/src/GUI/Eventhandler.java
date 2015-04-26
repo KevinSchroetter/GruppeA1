7d1474a8357dd595113651c258e5497c184cda85
AlexBrueckner <Alexander.Brueckner@Student.Reutlingen-University.de>
2015-04-27 01:21:35 +0200

    #map (hashMap) für Eventhandler drin

--- MADN/src/GUI/Eventhandler.java
+++ MADN/src/GUI/Eventhandler.java
@@ -3,24 +3,18 @@ package GUI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 
 public class Eventhandler implements ActionListener {
 	
-	private ArrayList<JButton> elements = null;
-	private JButton[] listeAlsArray;
+	private HashMap eventMap = null;
 	
 	
-	public Eventhandler(ArrayList<JButton> elements){
-		if(elements==null) throw new IllegalArgumentException("Elementliste fehlerhaft");
-		else this.elements = elements;
-		listeAlsArray = (JButton[]) elements.toArray();
-		
-		for(int i = 0; i<listeAlsArray.length; i++){
-			if(listeAlsArray[i] == null) continue;
-			else listeAlsArray[i].addActionListener(this);
-		}
+	public Eventhandler(HashMap eventMap){
+		if(eventMap == null) throw new IllegalArgumentException("Hashmap fehlerhaft");
+		else this.eventMap = eventMap;
 	}
 
 	@Override
